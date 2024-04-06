a78bfa5fcd1098e6d3912001eb18f5b47add4dc1
Kovács-Bodó Csenge <h160431@stud.u-szeged.hu>
2024-04-06 23:30:44 +0200

    missing

---
+++ blindmemorygame/src/app/services/game.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+  private timerSource = new BehaviorSubject<number>(90);
+  currentTimer = this.timerSource.asObservable();
+
+  private pointsSource = new BehaviorSubject<number>(0);
+  currentPoints = this.pointsSource.asObservable();
+
+  changeTimer(timer: number) {
+    this.timerSource.next(timer);
+  }
+
+  changePoints(points: number) {
+    this.pointsSource.next(points);
+  }
+}
